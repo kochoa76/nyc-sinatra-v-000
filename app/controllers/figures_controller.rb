54376c90ea4b09cf5f1b80b19fe3e9b1348cc01f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-23 15:17:55 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -33,7 +33,7 @@ class FiguresController < ApplicationController
      redirect to "/figures/#{@figure.id}"
    end
 
-   patch '/figures/:id/' do
+   post '/figures/:id/' do
        @figure = Figure.find(params[:id])
        @figure.update(params[:figure])
 
