f1313fc7cd0d4306ca23f0831d10d7820a6076e2
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-23 16:15:02 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -20,7 +20,7 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks/:id' do
-end 
+end
 
   post '/landmarks' do
     @landmark= Landmark.create(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
