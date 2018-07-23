06c0244551c72ac9e2feebae0997dbdbae9a227f
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-23 15:17:16 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -19,23 +19,6 @@ class FiguresController < ApplicationController
       erb :'figures/edit'
     end
 
-  post '/figures/:id/' do
-      @figure = Figure.find(params[:id])
-      @figure.update(params[:figure])
-      
-      if !params[:title][:name].empty?
-        @figure.titles << Figure.create(params[:title][:name])
-      end
-
-      if !params[:landmark][:name].empty?
-        @figure.landmarks << Landmark.create(params[:landmark][:title])
-      end
-      @figure.save
-      redirect to "/figures/#{@figure.id}"
-    end
-
-
-
   post '/figures' do
      @figure = Figure.create(params["figure"])
      if !params[:landmark][:name].empty?
@@ -49,4 +32,20 @@ class FiguresController < ApplicationController
      @figure.save
      redirect to "/figures/#{@figure.id}"
    end
+
+   post '/figures/:id/' do
+       @figure = Figure.find(params[:id])
+       @figure.update(params[:figure])
+
+       if !params[:title][:name].empty?
+         @figure.titles << Figure.create(params[:title][:name])
+       end
+
+       if !params[:landmark][:name].empty?
+         @figure.landmarks << Landmark.create(params[:landmark][:title])
+       end
+       @figure.save
+       redirect to "/figures/#{@figure.id}"
+     end
+
  end
